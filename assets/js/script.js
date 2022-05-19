3b40f8f21fc8afd770b615facccadd3ac0807c0b
infosecmentor <muhambiphares@gmail.com>
2022-05-19 14:42:43 +0300

    added login and index pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,3 +182,10 @@ function validate() {
     }
     return isValid;
 }
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
